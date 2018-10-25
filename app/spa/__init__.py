0666358a0732f8b76e6a928cbfd07d7fe37669f3
xgebi <sarah@sarahgebauer.com>
2018-10-25 11:37:45 +0200

    login styles

--- app/spa/__init__.py
+++ app/spa/__init__.py
@@ -1,3 +1,7 @@
+# SPA init file
+# version 0.0.1
+# author Sarah Gebauer
+
 from flask import Blueprint
 spa = Blueprint('spa', __name__, template_folder='templates')
 
--- app/static/main.js
+++ app/static/main.js
@@ -1,1 +1,18 @@
-// here is code handling SPA
+/**
+ * Main file with code necessary for SPA to work
+ * 
+ * version 0.0.1
+ * author Sarah Gebauer
+ */
+
+var notLoggedIn = true;
+var spa = document.querySelector("#spa");
+
+if (!notLoggedIn) {
+  let loginButton = spa.querySelector("#login-button");
+  loginButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log(window.location.href);
+    //fetch(window.location.href + );
+  });
+}
