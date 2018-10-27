35a647767c0085b97645ef7914d549f619c882b3
xgebi <sarah@sarahgebauer.com>
2018-10-28 00:32:35 +0200

    cleanup

--- app/spa/routes.py
+++ app/spa/routes.py
@@ -7,12 +7,4 @@ from app.settings.models import Setting
 @spa.route('/')
 def display_spa():
   configured = Setting.query.get('configured')
-  if (configured is not None and configured.settings_value):
-    return render_template('index.html', configured=configured)
-  else:
-    return render_template('configure.html')
-
-@spa.route('/register')
-def registration():
-  configured = Setting.query.get('registration_allowed')
-  return render_template('register.html')
+  return render_template('index.html', configured=configured)
